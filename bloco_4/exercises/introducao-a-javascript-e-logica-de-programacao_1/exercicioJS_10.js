0fbb02d0a948e8ea14941bcc8a16f96167bd9c2e
Fernanda Marques <nandateixmarques@gmail.com>
2020-10-25 21:43:36 -0300

    adiciona os exercicios 10 e 11

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicioJS_10.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicioJS_10.js
@@ -1,9 +1,9 @@
-let custoProduto = 20;
+let custoProduto = 28;
 let precoVenda = 100;
-let custoTotal = custoProduto + (custoProduto * 20/100);
-let lucroM = 1000 * (precoVenda - custoTotal);
 if (custoProduto <= 0 || precoVenda <= 0) {
     console.log("Valor inválido!!");
 }else {
+    let custoTotal = custoProduto + (custoProduto * 20/100);
+    let lucroM = 1000 * (precoVenda - custoTotal);
     console.log(lucroM);
 }
